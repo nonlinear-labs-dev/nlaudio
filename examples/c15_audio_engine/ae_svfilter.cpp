ba15bb090dc93a3f5269c7a7c866f1876e268081
Anton Schmied <aschmied@localhost.localdomain>
2018-07-24 17:07:09 +0200

    SVF: using std clamp, max, min and abs no

--- examples/c15_audio_engine/ae_svfilter.cpp
+++ examples/c15_audio_engine/ae_svfilter.cpp
@@ -59,7 +59,7 @@ void ae_svfilter::applySVFilter(float _sampleA, float _sampleB, float _sampleCom
     float tmpVar = _signal[SVF_AB];
     float firstSample = _sampleB * (1.f - tmpVar) + _sampleA * tmpVar;
     tmpVar = _signal[SVF_CMIX];
-    firstSample = firstSample * (1.f - fabs(tmpVar)) + _sampleComb * tmpVar;
+    firstSample = firstSample * (1.f - std::abs(tmpVar)) + _sampleComb * tmpVar;
 
     float secondSample = firstSample * _signal[SVF_PAR_4];
     secondSample += (m_first_sv_sample * _signal[SVF_PAR_3]);
@@ -74,11 +74,7 @@ void ae_svfilter::applySVFilter(float _sampleA, float _sampleB, float _sampleCom
 
     //************************** 1st Stage SV FILTER *************************//
     float omega = (_signal[SVF_F1_CUT] + tmpVar * _signal[SVF_F1_FM]) * m_warpConst_2PI;
-
-    if (omega > 1.9f)
-    {
-        omega = 1.9f;
-    }
+    omega = std::min(omega, 1.9f);
 
     float firOut = (m_first_fir_stateVar + firstSample) * 0.25f;
     m_first_fir_stateVar = firstSample + DNC_CONST;
@@ -95,35 +91,19 @@ void ae_svfilter::applySVFilter(float _sampleA, float _sampleB, float _sampleCom
     m_first_int1_stateVar = int1Out + DNC_CONST;
     m_first_int2_stateVar = int2Out + DNC_CONST;
 
-    m_first_sv_sample  =  lowpassOutput  * NlToolbox::Clipping::floatMax(-(_signal[SVF_LBH_1]), 0);
-    m_first_sv_sample += (bandpassOutput * (1.f - fabs(_signal[SVF_LBH_1])));
-    m_first_sv_sample += (highpassOutput * NlToolbox::Clipping::floatMax(_signal[SVF_LBH_1], 0));
+    m_first_sv_sample  =  lowpassOutput  * std::max(-(_signal[SVF_LBH_1]), 0.f);
+    m_first_sv_sample += (bandpassOutput * (1.f - std::abs(_signal[SVF_LBH_1])));
+    m_first_sv_sample += (highpassOutput * std::max(_signal[SVF_LBH_1], 0.f));
 
 
     //************************** 1st Stage Parabol Sat ***********************//
-    if (bandpassOutput > 2.f)
-    {
-        m_first_sat_stateVar = 2.f;
-    }
-    else if (bandpassOutput < -2.f)
-    {
-        m_first_sat_stateVar = -2.f;
-    }
-    else
-    {
-        m_first_sat_stateVar = bandpassOutput;
-    }
-
-    m_first_sat_stateVar *= (1.f - fabs(m_first_sat_stateVar) * 0.25f);
+    m_first_sat_stateVar = std::clamp(bandpassOutput, -2.f, 2.f);
+    m_first_sat_stateVar *= (1.f - std::abs(m_first_sat_stateVar) * 0.25f);
 
 
     //************************** 2nd Stage SV FILTER *************************//
     omega = (_signal[SVF_F2_CUT] + tmpVar * _signal[SVF_F2_FM]) * m_warpConst_2PI;
-
-    if (omega > 1.9f)
-    {
-        omega = 1.9f;
-    }
+    omega = std::min(omega, 1.9f);
 
     firOut = (m_second_fir_stateVar + secondSample) * 0.25f;
     m_second_fir_stateVar = secondSample + DNC_CONST;
@@ -140,26 +120,14 @@ void ae_svfilter::applySVFilter(float _sampleA, float _sampleB, float _sampleCom
     m_second_int1_stateVar = int1Out + DNC_CONST;
     m_second_int2_stateVar = int2Out + DNC_CONST;
 
-    tmpVar  =  lowpassOutput  * NlToolbox::Clipping::floatMax(-_signal[SVF_LBH_2], 0);
-    tmpVar += (bandpassOutput * (1.f - fabs(_signal[SVF_LBH_2])));
-    tmpVar += (highpassOutput * NlToolbox::Clipping::floatMax(_signal[SVF_LBH_2], 0));
+    tmpVar  =  lowpassOutput  * std::max(-_signal[SVF_LBH_2], 0.f);
+    tmpVar += (bandpassOutput * (1.f - std::abs(_signal[SVF_LBH_2])));
+    tmpVar += (highpassOutput * std::max(_signal[SVF_LBH_2], 0.f));
 
 
     //************************* 2nd Stage Parabol Sat ************************//
-    if (bandpassOutput > 2.f)
-    {
-        m_second_sat_stateVar = 2.f;                      // Clip Max 2.f
-    }
-    else if (bandpassOutput < -2.f)
-    {
-        m_second_sat_stateVar = -2.f;                     // Clip Min -2.f
-    }
-    else
-    {
-        m_second_sat_stateVar = bandpassOutput;
-    }
-
-    m_second_sat_stateVar *= (1.f - fabs(m_second_sat_stateVar) * 0.25f);
+    m_second_sat_stateVar = std::clamp(bandpassOutput, -2.f, 2.f);
+    m_second_sat_stateVar *= (1.f - std::abs(m_second_sat_stateVar) * 0.25f);
 
 
     //****************************** Crossfades ******************************//
@@ -178,19 +146,11 @@ void ae_svfilter::setSVFilter(float *_signal, float _samplerate)
     float resonance = _signal[SVF_RES];
     resonance = 1.f - resonance;
     resonance += resonance;
-
-    if (resonance < 0.02f)
-    {
-        resonance = 0.02f;
-    }
+    resonance = std::max(resonance, 0.02f);
 
     //****************************** 1st Stage ******************************//
     float omega = _signal[SVF_F1_CUT] * m_warpConst_2PI;
-
-    if (omega > 1.9f)
-    {
-        omega = 1.9;
-    }
+    omega = std::min(omega, 1.9f);
 
     m_first_attenuation = ((2.f + omega) * (2.f - omega) * resonance)
                         / (((resonance * omega) + (2.f - omega)) * 2.f);
@@ -198,11 +158,7 @@ void ae_svfilter::setSVFilter(float *_signal, float _samplerate)
 
     //****************************** 2nd Stage ******************************//
     omega = _signal[SVF_F2_CUT] * m_warpConst_2PI;
-
-    if (omega > 1.9f)
-    {
-        omega = 1.9;
-    }
+    omega = std::min(omega, 1.9f);
 
     m_second_attenuation = ((2.f + omega) * (2.f - omega) * resonance)
                          / (((resonance * omega) + (2.f - omega)) * 2.f);
