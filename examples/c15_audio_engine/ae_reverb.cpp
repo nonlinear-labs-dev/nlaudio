8d19f74d3c6cbca49e9841a1c2b3e2c8d84f00c0
Anton Schmied <aschmied@localhost.localdomain>
2018-08-02 18:20:24 +0200

    REV: under construction

--- examples/c15_audio_engine/ae_reverb.cpp
+++ examples/c15_audio_engine/ae_reverb.cpp
@@ -29,13 +29,27 @@ void ae_reverb::init(float _samplerate, uint32_t _upsampleFactor)
 {
     m_out_L = 0.f;
     m_out_R = 0.f;
+    m_out_FX = 0.f;
+
+
+    //************************** Reverb Modulation ***************************//
+    m_lfo_omega_1 = 0.86306f / _samplerate;
+    m_lfo_omega_2 = 0.6666f / _samplerate;
+
+    //****************************** Loop Filter *****************************//
+    m_warpConst_PI = pi / _samplerate;
+    m_omegaClip_max = _samplerate / 2.f;
+
+    m_lp_stateVar_L = 0.f;
+    m_lp_stateVar_R = 0.f;
+    m_hp_stateVar_L = 0.f;
+    m_hp_stateVar_R = 0.f;
 
     //***************************** Delay Buffer *****************************//
     m_buffer_indx = 0;
 
     m_buffer_sz_m1 = REVERB_BUFFER_SIZE * _upsampleFactor - 1;
     m_buffer_sz_m2 = REVERB_BUFFER_SIZE * _upsampleFactor - 2;
-
 
     m_buffer_L.resize(REVERB_BUFFER_SIZE * _upsampleFactor);
     m_buffer_L1.resize(REVERB_BUFFER_SIZE * _upsampleFactor);
@@ -100,7 +114,40 @@ void ae_reverb::init(float _samplerate, uint32_t _upsampleFactor)
     m_StateVar_R7 = 0.f;
     m_StateVar_R8 = 0.f;
     m_StateVar_R9 = 0.f;
+}
 
+
+
+/******************************************************************************/
+/** @brief  Chorus
+*******************************************************************************/
+
+void ae_reverb::set(float *_signal)
+{
+    float tmpVar;
+
+    tmpVar = _signal[REV_SIZE];
+    m_depth = _signal[REV_CHO] * (tmpVar * -200.f + 311.f);         /// Smoothing??
+
+    tmpVar = tmpVar * (0.5f - std::abs(tmpVar * -0.5f));
+    m_absorb  = tmpVar * 0.334f + 0.666f;                           /// Smoothing??
+    m_fb_amnt = tmpVar * 0.667f + 0.333f;                           /// Smoothing??
+
+    tmpVar = _signal[REV_PRE];
+    m_preDel_time_L = std::round(tmpVar);                           /// Smoothing?
+    m_preDel_time_R = std::round(tmpVar * 1.18933f);                /// Smoothing?
+
+    tmpVar = std::clamp(_signal[REV_LPF], 0.1f, m_omegaClip_max);
+    m_lp_omega = NlToolbox::Math::tan(tmpVar * m_warpConst_PI);     /// Smoothing??
+
+    m_lp_a0 = 1.f / (m_lp_omega + 1.f);
+    m_lp_a1 = m_lp_omega - 1.f;
+
+    tmpVar = std::clamp(_signal[REV_HPF], 0.1f, m_omegaClip_max);
+    m_hp_omega = NlToolbox::Math::tan(tmpVar * m_warpConst_PI);     /// Smoothing??
+
+    m_hp_a0 = 1.f / (m_hp_omega + 1.f);
+    m_hp_a1 = m_hp_omega - 1.f;
 }
 
 
@@ -112,22 +159,111 @@ void ae_reverb::init(float _samplerate, uint32_t _upsampleFactor)
 void ae_reverb::apply(float _rawSample_L, float _rawSample_R, float *_signal, float _fadePoint)
 {
     float tmpVar;
-//    int32_t ind_tm1, ind_t0, ind_tp1, ind_tp2;
+    float mod_1a, mod_2a, mod_1b, mod_2b;
+    int32_t ind_t0, ind_tm1, ind_tp1, ind_tp2;
+    float wetSample_L, wetSample_R;
+
+    //************************** Reverb Modulation ***************************//
+    /// Half Clock!!! Smoothing here!?!?
+    ///
+    tmpVar = m_lfo_stateVar_1 + m_lfo_omega_1;
+    tmpVar = tmpVar - std::round(tmpVar);
+    m_lfo_stateVar_1 = tmpVar;
+
+    tmpVar = (8.f - std::abs(tmpVar) * 16.f) * tmpVar;
+    tmpVar += 1.f;
+    mod_1a = tmpVar * m_depth;
+    mod_2a = (1.f - tmpVar) * m_depth;
+
+    tmpVar = m_lfo_stateVar_2 + m_lfo_omega_2;
+    tmpVar = tmpVar - std::round(tmpVar);
+    m_lfo_stateVar_2 = tmpVar;
+
+    tmpVar = (8.f - std::abs(tmpVar) * 16.f) * tmpVar;
+    tmpVar += 1.f;
+    mod_1b = tmpVar * m_depth;
+    mod_2b = (1.f - tmpVar) * m_depth;
 
 
-    //**************************** Left Channel *****************************//
-    tmpVar  = _rawSample_L * 1.f;            ///_signal[REV_FEED]
-    tmpVar *= _fadePoint;
 
-    m_buffer_L[m_buffer_indx] = tmpVar;
+    //***************************** Left Channel *****************************//
+    wetSample_L  = _rawSample_L * _signal[REV_FEED];
+
+    wetSample_L *= _fadePoint;                          // Asym
+    m_buffer_L[m_buffer_indx] = wetSample_L;
+
+    tmpVar = std::clamp(m_preDel_time_L, 0.f, static_cast<float>(m_buffer_sz_m1));
+
+    ind_t0 = static_cast<int32_t>(std::round(tmpVar - 0.5f));
+    tmpVar = tmpVar - static_cast<float>(ind_t0);
+
+    ind_tm1 = ind_t0 - 1;
+
+    ind_t0  = m_buffer_indx - ind_t0;
+    ind_tm1 = m_buffer_indx - ind_tm1;
+
+    ind_t0  &= m_buffer_sz_m1;
+    ind_tm1 &= m_buffer_sz_m1;
+
+    wetSample_L = m_buffer_L[ind_t0] + tmpVar * (m_buffer_L[ind_tm1] - m_buffer_L[ind_t0]);
+    wetSample_L *= _fadePoint;
+
+    wetSample_L += (m_StateVar_L9 * m_fb_amnt);
+
+    wetSample_L = (wetSample_L - m_lp_stateVar_L * m_lp_a1) * m_lp_a0;      // Loop Filter
+    tmpVar = m_lp_stateVar_L;
+    m_lp_stateVar_L = wetSample_L;
+
+    wetSample_L = (wetSample_L + tmpVar) * m_lp_omega;
+
+    wetSample_L = (wetSample_L - m_hp_stateVar_L * m_hp_a1) * m_hp_a0;
+    tmpVar = m_hp_stateVar_L;
+    m_hp_stateVar_L = wetSample_L;
+
+    wetSample_L = wetSample_L - tmpVar;
 
 
+    //**************************** Right Channel *****************************//
+    wetSample_R  = _rawSample_R * _signal[REV_FEED];
+
+    wetSample_R *= _fadePoint;                          // Asym
+    m_buffer_R[m_buffer_indx] = wetSample_R;
+
+    tmpVar = std::clamp(m_preDel_time_R, 0.f, static_cast<float>(m_buffer_sz_m1));
+
+    ind_t0 = static_cast<int32_t>(std::round(tmpVar - 0.5f));
+    tmpVar = tmpVar - static_cast<float>(ind_t0);
+
+    ind_tm1 = ind_t0 - 1;
+
+    ind_t0  = m_buffer_indx - ind_t0;
+    ind_tm1 = m_buffer_indx - ind_tm1;
+
+    ind_t0  &= m_buffer_sz_m1;
+    ind_tm1 &= m_buffer_sz_m1;
+
+    wetSample_R = m_buffer_R[ind_t0] + tmpVar * (m_buffer_R[ind_tm1] - m_buffer_R[ind_t0]);
+    wetSample_R *= _fadePoint;
+
+    wetSample_R += (m_StateVar_R9 * m_fb_amnt);
+
+    wetSample_R = (wetSample_R - m_lp_stateVar_R * m_lp_a1) * m_lp_a0;      // Loop Filter
+    tmpVar = m_lp_stateVar_R;
+    m_lp_stateVar_R = wetSample_R;
+
+    wetSample_R = (wetSample_R + tmpVar) * m_lp_omega;
+
+    wetSample_R = (wetSample_R - m_hp_stateVar_R * m_hp_a1) * m_hp_a0;
+    tmpVar = m_hp_stateVar_R;
+    m_hp_stateVar_R = wetSample_R;
+
+    wetSample_R = wetSample_R - tmpVar;
 
 
-    //*************************** Right Channel *****************************//
-    tmpVar  = _rawSample_R * 1.f;            ///_signal[REV_FEED]
-    tmpVar *= _fadePoint;
+    //**************************** Output Mixer ******************************//
+    m_out_L = _rawSample_L * _signal[REV_DRY] + wetSample_L * _signal[REV_WET];
+    m_out_R = _rawSample_R * _signal[REV_DRY] + wetSample_R * _signal[REV_WET];
 
-    m_buffer_R[m_buffer_indx] = tmpVar;
-
+    m_out_FX = ((_rawSample_L + _rawSample_R) * (1.f - _signal[FBM_REV]))
+            + ((wetSample_L + wetSample_R) * _signal[FBM_REV]);
 }
--- examples/c15_audio_engine/ae_reverb.h
+++ examples/c15_audio_engine/ae_reverb.h
@@ -13,6 +13,8 @@
 #include "nltoolbox.h"
 #include <vector>
 
+using namespace NlToolbox::Constants;
+
 
 #define REVERB_BUFFER_SIZE 16384
 
@@ -20,15 +22,36 @@ struct ae_reverb
 {
     ae_reverb();        // Default Contructor
 
-    float m_out_L, m_out_R;
+    float m_out_L, m_out_R, m_out_FX;
 
     void init(float _samplerate, uint32_t _upsampleFactor);
+    void set(float *_signal);
     void apply(float _rawSample_L, float _rawSample_R, float *_signal, float _fadePoint);
 
 
+    //************************** Reverb Modulation ***************************//
+    float m_lfo_omega_1, m_lfo_omega_2;
+    float m_lfo_stateVar_1, m_lfo_stateVar_2;
+    float m_depth;
+
+    //****************************** Loop Filter *****************************//
+    float m_warpConst_PI;
+    float m_omegaClip_max;
+
+    float m_lp_a0, m_lp_a1, m_lp_omega;
+    float m_hp_a0, m_hp_a1, m_hp_omega;
+
+    float m_lp_stateVar_L, m_lp_stateVar_R;
+    float m_hp_stateVar_L, m_hp_stateVar_R;
+
     //***************************** Delay Buffer *****************************//
+    float m_fb_amnt;
+    float m_absorb;
+
     int32_t m_buffer_indx;
     int32_t m_buffer_sz_m1, m_buffer_sz_m2;
+
+    float m_preDel_time_L, m_preDel_time_R;
 
     std::vector<float> m_buffer_L;
     std::vector<float> m_buffer_R;
